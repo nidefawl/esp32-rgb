86c51b614b1a2b61089fd8bbf60a017473cca669
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-10-24 00:33:01 +0200

    Cleanup the controller script

--- rgb-controller.py
+++ rgb-controller.py
@@ -1,8 +1,3 @@
-# a simple program that sends UDP packets to 192.168.188.43:54321
-
-# it packs the data into a struct and sends it over the network
-
-
 import socket
 import time
 import struct
@@ -10,35 +5,27 @@ import random
 import sys
 import math
 
+# A simple script that configures  the device and sends an animation
+
+LED_CONTROLLER_IP = "192.168.188.10"
+LED_CONTROLLER_UDP_PORT = 54321
+
 LED_NUM_STRIPES = 10
 LED_NUM_LEDS_PER_STRIPE = 30
 
-# define the packet types
 PKT_TYPE_HEARTBEAT = 0
 PKT_TYPE_CONFIG = 1
 PKT_TYPE_LED_FRAME = 2
 
-# define the config types
 CFG_ID_MAX_BRIGHTNESS = 1
 CFG_ID_FRAME_RATE = 2
 CFG_ID_STRIPES_ENABLE = 3
 CFG_NUM_CONFIG = 4
-
-# define the network settings
-UDP_IP = "192.168.188.10"
-UDP_PORT = 54321
-
-# define the packet header, pay attention to the byte order and size of fields
+# packet defs relating to https://github.com/nidefawl/esp32-rgb/blob/main/main/rgb-network-types.h
 packet_hdr = struct.Struct("<H H")
-
-# define the heartbeat message, pay attention to the byte order and size of fields
 heartbeat_message = struct.Struct("<Q")
-
-# define the config message, pay attention to the byte order and size of fields
 config_message = struct.Struct("<I I")
-
-# define the led frame message, pay attention to the byte order and size of fields
-# the pointer to the frame data is not included in the struct, it is added later
+# led frame message: frame size, frame id, then led data. LED data is 4 bytes per LED (RGBW) and appended after 
 led_frame_message = struct.Struct("<I I")
 
 
@@ -46,6 +33,7 @@ FPS = 100
 MAX_BRIGHTNESS = 80
 frame_id = 0
 
+# creates a rainbow circle pattern
 def process_pixels_rainbow_circle(time_since, frame_id, state):
     led_frame_data = led_frame_message.pack(
         LED_NUM_STRIPES * LED_NUM_LEDS_PER_STRIPE, 0
@@ -87,67 +75,45 @@ def process_pixels_rainbow_circle(time_since, frame_id, state):
             else:
                 r = 255
                 b = (360 - hue) / 60 * 255
-
-            # add the RGB values to led_frame_data
             led_frame_data += struct.pack("BBBB", int(r), int(g), int(b), int(w))
     return led_frame_data
 
+# creates a high frequency white noise pattern, which is just random colors
 def process_pixels_white_noise(time_since, frame_id, state):
     led_frame_data = led_frame_message.pack(
         LED_NUM_STRIPES * LED_NUM_LEDS_PER_STRIPE, 0
     )
-
     for stripe in range(LED_NUM_STRIPES):
         for led in range(LED_NUM_LEDS_PER_STRIPE):
             r = random.randint(0, 255)
             g = random.randint(0, 255)
             b = random.randint(0, 255)
             w = random.randint(0, 255)
-            # add the RGB values to led_frame_data
             led_frame_data += struct.pack("BBBB", int(r), int(g), int(b), int(w))
-    return led_frame_data
-
-def process_pixels_spiral(time_since, frame_id, state):
-    led_frame_data = led_frame_message.pack(
-        LED_NUM_STRIPES * LED_NUM_LEDS_PER_STRIPE, 0
-    )
-    
-    if "spiral" not in state:
-        state["spiral"] = {"pos": (0,0)}
-
-    spiral_state = state["spiral"]
-
-    empty_pixels = [0] * LED_NUM_STRIPES * LED_NUM_LEDS_PER_STRIPE
-    # empty_pixels[10*10] = 0xFF00FFFF
-
-    for stripe in range(LED_NUM_STRIPES):
-        for led in range(LED_NUM_LEDS_PER_STRIPE):
-            rgb = empty_pixels[stripe * LED_NUM_LEDS_PER_STRIPE + led]
-            # pack the 32 bit int directly 
-            led_frame_data += struct.pack(">I", rgb)
     return led_frame_data
 
 def main():
     global frame_id
-    # create socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # create heartbeat packet
+    # heartbeat has no function right now, send it anyways
     heartbeat_packet = packet_hdr.pack(PKT_TYPE_HEARTBEAT, heartbeat_message.size)
     heartbeat_packet += heartbeat_message.pack(random.randint(0, 1000000))
-    # send heartbeat packet
-    sock.sendto(heartbeat_packet, (UDP_IP, UDP_PORT))
+    sock.sendto(heartbeat_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
+    # configure the device
     config_packet = packet_hdr.pack(PKT_TYPE_CONFIG, config_message.size)
     config_packet += config_message.pack(CFG_ID_MAX_BRIGHTNESS, MAX_BRIGHTNESS)
-    sock.sendto(config_packet, (UDP_IP, UDP_PORT))
+    sock.sendto(config_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
     config_packet = packet_hdr.pack(PKT_TYPE_CONFIG, config_message.size)
     config_packet += config_message.pack(CFG_ID_FRAME_RATE, FPS)
-    sock.sendto(config_packet, (UDP_IP, UDP_PORT))
+    sock.sendto(config_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
     time_start = time.time()
+
+    # set the color of the LEDs to a specific color
     set_fullscreen_color(sock, 133, 133, 133)
-    # set_fullscreen_color(sock, 133, 33, 133)
+
     state = {}
     while 1:
         time_since = time.time() - time_start
@@ -159,8 +125,8 @@ def main():
             packet_hdr.pack(PKT_TYPE_LED_FRAME, size_led_frame_data)
         )
         led_frame_packet += led_frame_data
-        # send the led frame packet
-        sock.sendto(led_frame_packet, (UDP_IP, UDP_PORT))
+
+        sock.sendto(led_frame_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
         # sleep according to the frame rate
         time.sleep(1.0 / FPS)
@@ -180,8 +146,7 @@ def set_fullscreen_color(sock, r, g, b):
         packet_hdr.pack(PKT_TYPE_LED_FRAME, size_led_frame_data)
     )
     led_frame_packet += led_frame_data
-    # send the led frame packet
-    sock.sendto(led_frame_packet, (UDP_IP, UDP_PORT))
+    sock.sendto(led_frame_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
 
 if __name__ == "__main__":
