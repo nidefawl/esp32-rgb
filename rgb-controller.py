576af761ffb675c26c51e9e5ca7e372cf4342252
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-10-26 22:45:54 +0200

    Fix bidirectional frame requests

--- rgb-controller.py
+++ rgb-controller.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import time
 import struct
 import random
@@ -18,6 +19,7 @@ CFG_ID_MAX_BRIGHTNESS = 1
 CFG_ID_FRAME_RATE = 2
 CFG_ID_STRIPES_ENABLE = 3
 CFG_ID_ENABLE_DEBUG = 4
+CFG_ID_HEARTBEAT_INTERVAL_FRAMES = 5
 CFG_NUM_CONFIG = 4
 
 # network settings
@@ -28,8 +30,8 @@ LED_CONTROLLER_UDP_PORT = 54321
 # packet header, u16 packet type, u16 packet size
 packet_hdr = struct.Struct("<H H")
 
-# heartbeat message, u64 random number
-heartbeat_message = struct.Struct("<Q")
+# heartbeat message, u64 frame id, u64 fps, u64 heartbeat interval
+heartbeat_message = struct.Struct("<Q Q Q")
 
 # config message, u32 config id, u32 config value
 config_message = struct.Struct("<I I")
@@ -39,10 +41,14 @@ config_message = struct.Struct("<I I")
 led_frame_message = struct.Struct("<I I")
 
 
-FPS = 50
-MAX_BRIGHTNESS = 100
+FPS = 80
+MAX_BRIGHTNESS = 255
+HEARTBEAT_INTERVAL = 3
 frame_id = 0
-
+time_start = 0
+program_state = {}
+num_frames = 0
+USE_HEARTBEAT_FRAME_GEN = True
 
 def process_pixels_rainbow_circle(time_since, frame_id, state):
     led_frame_data = led_frame_message.pack(
@@ -113,15 +119,51 @@ def process_pixels_white_noise(time_since, frame_id, state):
             # add RGB values to led_frame_data
             led_frame_data += struct.pack("BBBB", int(b), int(g), int(r), int(w))
     return led_frame_data
+def gen_frame(sock, frame_id, program_state):
+    # tmNow = time.time()
+    time_since = frame_id / FPS
+    led_frame_data = process_pixels_rainbow_circle(time_since, frame_id, program_state)
 
+    size_led_frame_data = len(led_frame_data)
+    led_frame_packet = bytearray(
+        packet_hdr.pack(PKT_TYPE_LED_FRAME, size_led_frame_data)
+    )
+    led_frame_packet += led_frame_data
+    # send led frame packet
+    sock.sendto(led_frame_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
+
+# second thread function to recv heartbeats
+def recv_heartbeats(*args):
+    global frame_id, program_state, num_frames
+    sock = args[0]
+    while 1:
+        # data, addr = sock.recvfrom(1024)
+        # recvfrom UDP IP and port
+        data, addr = sock.recvfrom(1024)
+        if len(data) > heartbeat_message.size:
+            pkt_type, pkt_size = packet_hdr.unpack(data[:4])
+            if pkt_type == PKT_TYPE_HEARTBEAT and USE_HEARTBEAT_FRAME_GEN:
+                frame_id, fps, heartbeat_interval = heartbeat_message.unpack(data[4:])
+                # print("Received heartbeat", frame_id, fps, heartbeat_interval)
+                # generate heartbeat_interval frames
+                for i in range(heartbeat_interval):
+                    gen_frame(sock, frame_id, program_state)
+                    num_frames += 1
+                    frame_id += 1
+        else:
+            print("Received packet with invalid size", len(data))
 def main():
-    global frame_id
+    global frame_id, time_start, num_frames, program_state
     # create socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # create second thread to receive heartbeats using the same socket
+    thread = threading.Thread(target=recv_heartbeats, args=(sock,))
+    thread.start()
+
     # heartbeat has no function right now, send it anyways
     heartbeat_packet = packet_hdr.pack(PKT_TYPE_HEARTBEAT, heartbeat_message.size)
-    heartbeat_packet += heartbeat_message.pack(random.randint(0, 1000000))
+    heartbeat_packet += heartbeat_message.pack(frame_id, FPS, 0)
     sock.sendto(heartbeat_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
     # configure max brightness
@@ -145,33 +187,38 @@ def main():
     config_packet += config_message.pack(CFG_ID_ENABLE_DEBUG, int(1))
     # sock.sendto(config_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
 
+    # configure heartbeat interval
+    config_packet = packet_hdr.pack(PKT_TYPE_CONFIG, config_message.size)
+    config_packet += config_message.pack(CFG_ID_HEARTBEAT_INTERVAL_FRAMES, HEARTBEAT_INTERVAL)
+    if USE_HEARTBEAT_FRAME_GEN:
+      sock.sendto(config_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
+
+
     time_start = time.time()
     # br = 0.25 * 255
     # set_fullscreen_color(sock, br, br, br)
-    state = {}
     time_last_fps = time.time()
-    num_frames = 0
     time_last_frame = time.time()
     avg_fps = FPS
     while 1:
         tmNow = time.time()
         tmDelta = tmNow - time_last_frame
-        if tmDelta < (1.0/FPS) - 0.00098:
+        if tmDelta < (1.0/FPS) - 0.0008:
             time.sleep(0.001)
             continue
-        time_since = tmNow - time_start
-        led_frame_data = process_pixels_rainbow_circle(time_since, frame_id, state)
+        if not USE_HEARTBEAT_FRAME_GEN:
+            time_since = tmNow - time_start
+            led_frame_data = process_pixels_rainbow_circle(time_since, frame_id, program_state)
 
-        size_led_frame_data = len(led_frame_data)
-        led_frame_packet = bytearray(
-            packet_hdr.pack(PKT_TYPE_LED_FRAME, size_led_frame_data)
-        )
-        led_frame_packet += led_frame_data
-        # send led frame packet
-        sock.sendto(led_frame_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
+            size_led_frame_data = len(led_frame_data)
+            led_frame_packet = bytearray(
+                packet_hdr.pack(PKT_TYPE_LED_FRAME, size_led_frame_data)
+            )
+            led_frame_packet += led_frame_data
+            # send led frame packet
+            sock.sendto(led_frame_packet, (LED_CONTROLLER_IP, LED_CONTROLLER_UDP_PORT))
         time_last_frame = tmNow
         frame_id += 1
-        num_frames += 1
         if num_frames > 4 and tmNow - time_last_fps > 5.0:
             current_fps = num_frames / (tmNow - time_last_fps)
             avg_fps = 0.9 * avg_fps + 0.1 * current_fps
